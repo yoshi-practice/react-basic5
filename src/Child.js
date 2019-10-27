6e298e097a2f19c50d9b3cbd4e8aa93c4d2d09c1
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-10-27 12:27:16 +0900

    feat(coding): child

--- src/Child.js
+++ src/Child.js
@@ -6,4 +6,14 @@ class Child extends Component{
     addValueChild(){
         this.props.add(10);
     }
+
+    render(){
+        return(
+            <div>{this.props.pval}
+                <button onClick={this.addValueChild.bind(this)}>ADD</button>
+            </div>
+        )
+    }
 }
+
+export default Child;
